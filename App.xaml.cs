4136a80952485d23755c9d1197ca4ab5d1e27b15
yossi <yossi.buch@gmail.com>
2018-08-13 13:26:22 -0400

    init commit

---
+++ App.xaml.cs
@@ -0,0 +1,51 @@
+﻿using PrismTest.Views;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using System.Threading.Tasks;
+using Windows.ApplicationModel;
+using Windows.ApplicationModel.Activation;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+namespace PrismTest
+{
+    /// <summary>
+    /// Provides application-specific behavior to supplement the default Application class.
+    /// </summary>
+    sealed partial class App : Prism.Unity.Windows.PrismUnityApplication// Application
+    {
+        /// <summary>
+        /// Initializes the singleton application object.  This is the first line of authored code
+        /// executed, and as such is the logical equivalent of main() or WinMain().
+        /// </summary>
+        public App()
+        {
+            this.InitializeComponent();
+            //  this.Suspending += OnSuspending;
+        }
+
+        protected override Task OnLaunchApplicationAsync(LaunchActivatedEventArgs args)
+        {
+            NavigationService.Navigate(nameof(MainPage), null);
+            return Task.FromResult<object>(null);
+        }
+        //protected override Type GetPageType(string pageToken)
+        //{
+
+        //    var type = Type.GetType($"PrismTest.Views.{pageToken}");
+        //    return type;
+        //   // return base.GetPageType();
+        //}
+
+    }
+}
